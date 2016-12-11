0159ccfbc01bafbce573a8094879bb0be34ebd27
youleer727 <danicaliu727@gmail.com>
2016-12-10 19:02:36 -0800

    Update permutation.java

--- permutation.java
+++ permutation.java
@@ -1,23 +1,28 @@
 public class Solution {
   public List<List<Integer>> permute(int[] nums){
     ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>>();
-    if(nums == null){
+    if (nums == null || nums.length == 0) {
       return rst;
     }
-    
-    if(nums.length == 0){
-      rst.add(new ArrayList<Integer>());
-      return rst;
-    }
-    
+    boolean[] visited = new boolean[nums.length];
     ArrayList<Integer> list = new ArrayList<>();
-    helper(rst, list, nums);
+    helper(rst, list, nums, visited);
     return rst;
   }
-  public void helper(ArrayList<ArrayList<Interger>> rst, ArrayList<Integer> list, int[] nums){
+  public void helper(ArrayList<ArrayList<Interger>> rst, ArrayList<Integer> list, int[] nums, boolean[] visited){
     if(list.size() == nums.length){
       rst.add(new ArrayList<Integer>(list));
       return;
+    }
+    for (int i = 0; i < nums.length; i++){
+      if (visited[i] == true){
+        continue;
+      }
+      list.add(nums[i]);
+      visited[i] = true;
+      helper(rst, list, nums,);
+      list.remove(list.size() - 1);
+      visited[i] = false;
     }
   }
 }
